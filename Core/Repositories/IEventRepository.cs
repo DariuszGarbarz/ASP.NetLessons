24af7bdb50ed5a09ef8d71676c851fdeb1ded6aa
DariuszGarbarz <plgarbarda@gmail.com>
2022-01-13 16:16:43 +0100

    Repositories Interfaces created.

---
+++ Core/Repositories/IEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<Event> GetAsync(Guid id);
+        Task<Event> GetAsync(string name);
+
+        Task<IEnumerable<Event>> BrowseAsync(string name = "");
+
+        Task AddAsync(Event @event);
+        Task UpdateAsync(Event @event);
+        Task DeleteAsync(Event @event);
+    }
+}
