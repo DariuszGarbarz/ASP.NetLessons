59b842e154ea99b03cb1efe1674b2c8e4a6d4ac8
DariuszGarbarz <plgarbarda@gmail.com>
2022-01-13 15:47:38 +0100

    Dependencies between projects created, First models of Event, Ticket and User created and filled up

---
+++ Core/Domain/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain
+{
+    public class Ticket : Entity
+    {
+        public Guid EventId { get; protected set; }
+        public int Seating { get; protected set; }
+        public decimal Price { get; protected set; }
+        public Guid? UserId { get; protected set; }
+        public string Username { get; protected set; }
+        public DateTime? PutchasedAt { get; protected set; }
+
+        public bool IsPurchased => UserId.HasValue;
+
+        protected Ticket()
+        {
+        }
+        public Ticket(Event @event, int seating, decimal price)
+        {
+            EventId = @event.Id;
+            Seating = seating;
+            Price = price;
+            
+        }
+    }
+}
