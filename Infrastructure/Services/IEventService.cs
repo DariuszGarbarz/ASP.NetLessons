49cd751e1af3ed834194115d6c5d71efcecaab6f
DariuszGarbarz <plgarbarda@gmail.com>
2022-01-14 14:46:01 +0100

    Event DTO Added, Event Service interface completed

---
+++ Infrastructure/Services/IEventService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Domain;
+using Infrastructure.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public interface IEventService
+    {
+        Task<EventDto> GetAsync(Guid id);
+        Task<EventDto> GetAsync(string name);
+        Task<IEnumerable<EventDto>> BrowseAsync(string name = null);
+
+        Task CreateAsync(Guid id, string name, string description, DateTime startDate, DateTime endDate);
+        Task AddTicketsAsync(Guid eventId, int amount, decimal price);
+        Task UpdateAsync(Guid id, string name, string description);
+        Task DeleteAsync(Guid id);
+    }
+}
