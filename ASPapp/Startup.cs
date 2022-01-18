6ae883b28ac27b86bef487817ac7a525e706daf1
DariuszGarbarz <plgarbarda@gmail.com>
2022-01-18 16:34:43 +0100

    IOC Containers Added

--- ASPapp/Startup.cs
+++ ASPapp/Startup.cs
@@ -1,3 +1,7 @@
+using Core.Repositories;
+using Infrastructure.Mappers;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +20,10 @@ namespace ASPapp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IEventService, EventService>();
+            services.AddSingleton(AutoMapperConfig.Initialize());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
